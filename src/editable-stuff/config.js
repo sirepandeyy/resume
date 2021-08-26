fccd6d74e467d2765c79cb07922f05e4c44d14e2
Aman Kumar Pandey <44319659+sirepandeyy@users.noreply.github.com>
2021-08-26 10:39:48 +0530

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -73,7 +73,7 @@ const repos = {
 
 // Leadership SECTION
 const leadership = {
-  show: false,
+  show: true,
   heading: "Leadership",
   message:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
@@ -104,9 +104,12 @@ const skills = {
     { name: "SQL", value: 75 },
     { name: "Data Structures", value: 70 },
     { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 70 },
+    { name: "Java", value: 70 },
     { name: "React", value: 65 },
     { name: "HTML/CSS", value: 80 },
+    { name: "Api Development", value:60 },
+    { name: "Nestjs Orm", value: 70},
+    { name: "Nodejs", value:70 },
     { name: "JavaScript", value: 60 }
   ],
   softSkills: [
@@ -141,9 +144,9 @@ const experiences = {
   ]
 }
 
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
+ Blog SECTION
+ const blog = {
+   show: true,
+ };
 
 export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
